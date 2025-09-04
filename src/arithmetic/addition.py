2b660523d2737a0b11da805b3bcf57f615ca5a07
haessar <wahhill@gmail.com>
2025-09-04 16:07:57 +0100

    improve addition

--- src/arithmetic/addition.py
+++ src/arithmetic/addition.py
@@ -1,2 +1,17 @@
-def sum_two_integers(a: int, b: int) -> int:
-    return a + b
+from typing import Union, Iterable
+
+
+def sum_nested_ints(*args: Union[int, Iterable]) -> int:
+    output = 0
+    for i in args:
+        if isinstance(i, int):
+            output += i
+        elif isinstance(i, Iterable):
+            output += sum_nested_ints(*i)
+    return output
+
+
+# def sum_ints(xs):
+#     # This dtype name was removed in NumPy 1.24
+#     arr = np.asarray(xs, dtype=np.int)  # AttributeError on >=1.24
+#     return int(arr.sum())
--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -1,5 +1,6 @@
-from arithmetic.addition import sum_two_integers
+from arithmetic.addition import sum_nested_ints
 
-def test_add():
-    assert sum_two_integers(1, 2) == 3
-    assert sum_two_integers(2, 2) != 5
+def test_sum_nested_ints():
+    assert sum_nested_ints(1, 2) == 3
+    assert sum_nested_ints(2, 2) != 5
+    assert sum_nested_ints([1, 2], 3) == 6
