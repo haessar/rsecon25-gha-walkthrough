aa92fd93f345e01ba4aa9321167bfecd06e4b8a8
haessar <wahhill@gmail.com>
2025-09-07 11:58:18 +0100

    add numpy req to addition

---
+++ src/arithmetic/addition_0.py
@@ -0,0 +1,14 @@
+def sum_nested_ints(*args: int) -> int:
+    output = 0
+    for i in args:
+        if isinstance(i, int):
+            output += i
+        else:
+            output += sum_nested_ints(*i)
+    return output
+
+
+# def sum_ints(xs):
+#     # This dtype name was removed in NumPy 1.24
+#     arr = np.asarray(xs, dtype=np.int)  # AttributeError on >=1.24
+#     return int(arr.sum())
