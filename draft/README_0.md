7879502cb19df7c0475134ace8dc502f716b6a6b
haessar <wahhill@gmail.com>
2025-09-08 22:56:34 +0100

    remove redundants files

--- draft/README_0.md
+++
@@ -1,5 +0,0 @@
-# RSECon25
-
-## Automated CI/CD with GitHub Actions, starting from scratch
-
-This repo is a minimal Python package with tests from which we can build CI/CD workflows.
--- src/arithmetic/draft/addition_0.py
+++
@@ -1,8 +0,0 @@
-def sum_nested_ints(*args: int) -> int:
-    output = 0
-    for i in args:
-        if isinstance(i, int):
-            output += i
-        else:
-            output += sum_nested_ints(*i)
-    return output
